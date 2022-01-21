b3613d9cda947530e29294f651e928062678ce7b
Syed Ziauddin <Syed.nlp00@gmail.com>
2022-01-21 23:17:38 +0530

    hashed password

--- backend/models/register.js
+++ backend/models/register.js
@@ -1,42 +1,39 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+// const bcrypt = require("bcryptjs");
+
 
 //Schema
 
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true,
-        trim: true
+        required: true
     },
     email: {
         type: String,
         required: true,
-        unique: true,
-        trim: true
+        unique: true
     },
     password: {
         type: String,
-        required:true,
-        trim: true
+        required:true
     },
     cpassword : {
         type: String,
-        required: true,
-        trim: true
+        required: true
     }
 
 })
 
-//hashing password
-userSchema.pre('save',async function(next){
-    console.log('Hey from BcryptJS');
-    if(this.isModified('password')){
-        this.password = bcrypt.hash(this.password, 10);
-        this.cpassword = bcrypt.hash(this.cpassword, 10);
-    }
-    next();
-});
+//middleware
+// userSchema.pre('save', async function(next){
+//     console.log(`Hello from BcryptJs!`);
+//     if(this.isModified('password')){
+//         this.password = bcrypt.hash(this.password,12);
+//         this.cpassword = bcrypt.hash(this.cpassword,12);
+//     }
+//     next();
+// });
 
 //model 
 const Register =  mongoose.model("Register", userSchema);
--- backend/router/auth.js
+++ backend/router/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 require('../db/config');
 
 const Register = require('../models/register');
@@ -14,20 +15,25 @@ router.post('/register', async(req,res)=>{
     const { name, email, password, cpassword} = req.body;
 
     if(!name || !email || !password || !cpassword){
-        return res.status(422).json({Error:"Please fill every field!"}); 
+        return res.status(422).json({Error:"Input fields Can not be empty"});
     }
 
     try {
        const userExist = await Register.findOne({email: email})
 
        if(userExist) {
-        return res.status(422).json({error:"Email Already Exist"});
+        return res.status(422).json({error:"Email already Exist"});
     }else if(password != cpassword){
         return res.status(422).json({error:"Password does not match!"});
     }else{
         const register = new Register({name, email, password, cpassword});
+
+        //hashing
+        register.password =  await bcrypt.hash(register.password , 12);
+        register.cpassword =  await bcrypt.hash(register.cpassword , 12);
+
         await register.save();
-        return res.status(201).json({message:"user registered Successfully"});
+        return res.status(201).json({message:"User registered Successfully"});
         }
 
     }catch (err) {
@@ -36,4 +42,29 @@ router.post('/register', async(req,res)=>{
   
 });
 
-module.exports = router;
+//User login authentication 
+
+router.post('/login', async(req,res) =>{
+    const { email, password} = req.body;
+    if(!email || !password){
+         return res.status(400).json({Error:"Please enter email & password !"});
+    }
+
+    try {
+        const userEmail = await Register.findOne({email: email});
+        const userPassword = await Register.findOne({password:password});
+        if(userEmail && userPassword){
+            return res.status(200).json({message:"Login Successfully!"});
+        }else if(!userEmail){
+            return res.status(422).json({error:"This email is not registered!"});
+        }else if(!userEmail || !userPassword){
+            return res.status(422).json({error:"Invalide email and password Combinations"});
+        }
+  
+    } catch (err) {
+        console.log(err);
+    }
+
+});
+
+module.exports = router; 
