50c86569df9cb1e3500c9b0a89ff8109f940392d
Sriram <72183029+SriramSonaganti@users.noreply.github.com>
2022-02-04 13:07:55 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Login from './components/Login';
+import Login from './components/login';
 import Register from './components/register';
 import About from './components/about'
 import Home from './container/Home';
