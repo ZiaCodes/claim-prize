c8c3d149a476a04d7970cef2ebcc306a2fca69ae
unknown <pawaraniket508@gmail.com>
2022-02-10 18:35:02 +0530

    Added Landing Page

--- client/src/components/nav.jsx
+++ client/src/components/nav.jsx
@@ -1,29 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const nav = () => {
   return (
-      <div className='flex justify-between shadow-md  '>
-          <div>
-              <h1 className=' py-5 mx-2 font-bold text-emerald-600 text-xl'>CLAIM <span className='text-zinc-900'>SWAG</span></h1>
-          </div>
-          <div>
-              <ul className='flex'>
-                  <li className='font-bold px-5 my-6 text-xl hover:text-gray-500 cursor-pointer'>
-                      <Link to='/'>Home</Link>
-                  </li>
-                  <li className='font-bold px-5 my-6 text-xl hover:text-gray-500 cursor-pointer'>
-                  <Link to='/about'>About</Link>
-                  </li>
-                  <li className='font-bold px-5 my-6 text-xl hover:text-gray-500 cursor-pointer'>
-                  <Link to='/login'>Login</Link>
-                  </li>
-                  <li className='font-bold px-5 my-6 text-xl hover:text-gray-500 cursor-pointer'>
-                  <Link to='/register'>Register</Link>
-                  </li>
-              </ul>
-          </div>
+    <div className="flex justify-between px-6">
+      <div>
+        <h1 className="py-5 mx-2 font-bold text-emerald-600 text-xl">
+          CLAIM <span className="text-zinc-900">SWAG</span>
+        </h1>
       </div>
+      <div>
+        <ul className="flex">
+          <li className="font-bold px-5 my-6 text-xl hover:text-gray-500 cursor-pointer">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="font-bold px-5 my-6 text-xl hover:text-gray-500 cursor-pointer">
+            <Link to="/about">About</Link>
+          </li>
+          <li className="font-bold px-5 my-6 text-xl hover:text-gray-500 cursor-pointer">
+            <Link to="/login">Login</Link>
+          </li>
+          <li className="font-bold px-5 my-6 text-xl hover:text-gray-500 cursor-pointer">
+            <Link to="/register">Register</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
